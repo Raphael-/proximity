6a4a33640e8d9dd0559aa338e24b888e6227a5e7
r.magkos <r.magkos@gmail.com>
2020-05-08 22:52:48 +0300

    Initial commit

---
+++ proximity/src/main/java/gr/rmagkos/proximity/App.java
@@ -0,0 +1,13 @@
+package gr.rmagkos.proximity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
