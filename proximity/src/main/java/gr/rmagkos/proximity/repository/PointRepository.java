9b0131c55139700d7a7161d9db645161c22803eb
r.magkos <r.magkos@gmail.com>
2020-05-10 22:18:56 +0300

    Added javadoc

--- proximity/src/main/java/gr/rmagkos/proximity/repository/PointRepository.java
+++ proximity/src/main/java/gr/rmagkos/proximity/repository/PointRepository.java
@@ -8,9 +8,27 @@ import gr.rmagkos.proximity.model.Point;
  * Defines the set of operations a repository of this type should support
  */
 public interface PointRepository {
+	/**
+	 * Retrieves a {@link Point} that is closest to <code>pointToExamine</code>
+	 * 
+	 * @param pointToExamine
+	 * @return said {@link Point}
+	 */
 	Point fetchNearestPointTo(Point pointToExamine);
 
+	/**
+	 * Updates the hit counter of a {@link Point}
+	 * 
+	 * @param pointToUpdate
+	 */
 	void updateHitCount(Point pointToUpdate);
 
+	/**
+	 * Retrieves a list of {@link Point} that have hit count >=
+	 * <code>hitsCount</code>
+	 * 
+	 * @param hitsCount
+	 * @return
+	 */
 	List<Point> fetchPointsHavingHitsMoreThan(int hitsCount);
 }
--- proximity/src/main/java/gr/rmagkos/proximity/service/ProximityService.java
+++ proximity/src/main/java/gr/rmagkos/proximity/service/ProximityService.java
@@ -12,7 +12,21 @@ import gr.rmagkos.proximity.model.Point;
  */
 @WebService
 interface ProximityService {
+	/**
+	 * Retrieves the label (friendly name) of a {@link Point} that is nearest to the
+	 * <code>pointUnderCheck</code>
+	 * 
+	 * @param pointUnderCheck
+	 * @return
+	 */
 	String getLabelOfNearestPoint(Point pointUnderCheck);
 
+	/**
+	 * Retrieves a list of {@link Point} that have hit count >=
+	 * <code>hitsCount</code>
+	 * 
+	 * @param hitsCount
+	 * @return
+	 */
 	List<Point> fetchPointsHavingHitsMoreThan(int hitsCount);
 }
